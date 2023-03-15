e0f930c1464166aa715793fd039c35a49f8fdb3d
Debray Thomas <thomasdebray83@gmail.com>
2023-03-15 17:16:19 +0100

    Variable and scope

--- CSharpPractice/CSharpPractice/Program.cs
+++ CSharpPractice/CSharpPractice/Program.cs
@@ -2,10 +2,16 @@
 {
 	internal class Program
 	{
+		static double numberTwo = 12.34;
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
-			string message = Console.ReadLine();
+			if(numberTwo == 1.2)
+			{
+				var numberThree = 12f;
+			}
+			var numberOne = 23;
+			Console.WriteLine(numberTwo);
+			var message = Console.ReadLine();
 			Console.WriteLine(message);
 			Console.ReadLine();
 		}
