42d02d4380a91d6dc6135b3cb0b36cb24ffcf05f
Debray Thomas <thomasdebray83@gmail.com>
2023-03-16 10:33:35 +0100

    Class and NameSpace

--- CSharpPractice/CSharpPractice/Program.cs
+++ CSharpPractice/CSharpPractice/Program.cs
@@ -1,19 +1,21 @@
-﻿namespace CSharpPractice
+﻿using CSharpPractice.Classes;
+
+namespace CSharpPractice
 {
 	internal class Program
 	{
 		static double numberTwo = 12.34;
 		static void Main(string[] args)
 		{
-			if(numberTwo == 1.2)
-			{
-				var numberThree = 12f;
-			}
-			var numberOne = 23;
-			Console.WriteLine(numberTwo);
-			var message = Console.ReadLine();
-			Console.WriteLine(message);
-			Console.ReadLine();
+			
+		}
+	}
+
+	class SimpleMath
+	{
+		public static double Add(double n1, double n2)
+		{
+			return n1 + n2;
 		}
 	}
 }
