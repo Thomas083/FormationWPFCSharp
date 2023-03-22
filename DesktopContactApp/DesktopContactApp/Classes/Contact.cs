aceef0fa7a618004b200515657c41de782136133
Debray Thomas <thomasdebray83@gmail.com>
2023-03-22 09:47:09 +0100

    Update Contact.cs

--- DesktopContactApp/DesktopContactApp/Classes/Contact.cs
+++ DesktopContactApp/DesktopContactApp/Classes/Contact.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,8 @@ namespace DesktopContactApp.Classes
 {
     public class Contact
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
