ecc8c12412892eab0ff47c81bb5a7f793884dc5c
Debray Thomas <thomasdebray83@gmail.com>
2023-03-22 10:13:53 +0100

    Using Statement

--- DesktopContactApp/DesktopContactApp/NewContactWindow.xaml.cs
+++ DesktopContactApp/DesktopContactApp/NewContactWindow.xaml.cs
@@ -28,10 +28,11 @@ namespace DesktopContactApp
             string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             string databasePath = System.IO.Path.Combine(folderPath, databaseName);
 
-            SQLiteConnection connection = new SQLiteConnection(databasePath);
-            connection.CreateTable<Contact>();
-            connection.Insert(contact);
-            connection.Close();
+            using (SQLiteConnection connection = new SQLiteConnection(databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Insert(contact);
+            }
 
             Close();
         }
